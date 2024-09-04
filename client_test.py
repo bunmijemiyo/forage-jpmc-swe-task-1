3de498268f0f085d75e907eb43084c2b6722b80a
Olubunmi Ogunjemiyo <bjemiyor@gmail.com>
2024-09-04 12:43:34 +0100

    Update functions in client3.py, add assertions, and create additional test function in client_test.py. Add .gitignore to exclude virtual environment files.

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import (getDataPoint, getRatio)
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(price, (bid_price + ask_price) / 2)
+            self.assertEqual(stock, quote['stock'])
+            self.assertEqual(bid_price, quote['top_bid']['price'])
+            self.assertEqual(ask_price, quote['top_ask']['price'])
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +21,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(price, (bid_price + ask_price) / 2)
+            self.assertEqual(stock, quote['stock'])
+            self.assertEqual(bid_price, quote['top_bid']['price'])
+            self.assertEqual(ask_price, quote['top_ask']['price'])
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self):
+        self.assertEqual(getRatio(120.48, 119.2), 120.48 / 119.2)
+        self.assertEqual(getRatio(119.2, 119.2), 1)
+        self.assertIsNone(getRatio(119.2, 0))
 
 
 
